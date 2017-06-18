85d10e9bb7779123ff842d8b5315108783f52488
charra_s <charra_s@epitech.eu>
2017-06-18 18:45:29 +0200

    Fix Event Class with a enum

--- source/engine/event/eventReceiver.cpp
+++ source/engine/event/eventReceiver.cpp
@@ -5,7 +5,7 @@
 // Login   <sofiane@epitech.net>
 //
 // Started on  Tue May 30 21:39:51 2017 sofiane
-// Last update Sun Jun 18 18:40:51 2017 Melliti
+// Last update Sun Jun 18 18:44:23 2017 sofiane
 //
 
 #include "eventReceiver.hpp"
@@ -36,12 +36,6 @@ eventReceiver::inputCase	eventReceiver::lastKey(){
   return (eventReceiver::inputCase::NOT_FOUND);
 }
 
-// eventReceiver::inputCase	eventReceiver::recupInput(const irr::SEvent &event)
-// {
-//   if (event.EventType == eventReceiver::inputCase::KEY_KEY_W)
-//     return (inputCase);
-// }
-
 bool		eventReceiver::OnEvent(const irr::SEvent &event)
 {
   //  inputCase	input;
@@ -50,34 +44,28 @@ bool		eventReceiver::OnEvent(const irr::SEvent &event)
       switch (event.KeyInput.Key)
 	{
 	case eventReceiver::inputCase::KEY_KEY_W: // avancer
-	  //input = 1;
 	  eventReceiver::lastPressedKey = eventReceiver::inputCase::KEY_KEY_W;
 	  lastKey();
 	  return true;
 	case irr::KEY_KEY_S: // reculer
-	  // input = 2;
-	  std::cout << "Vous avez appuyez sur RECULER {S} !" << std::endl;
+	  //	  std::cout << "Vous avez appuyez sur RECULER {S} !" << std::endl;
 	  eventReceiver::lastPressedKey = eventReceiver::inputCase::KEY_KEY_S;
 	  return true;
 	case irr::KEY_KEY_A: // gauche
-	  //input = 3;
-	  std::cout << "Vous avez appuyez sur GAUCHE {A} !" << std::endl;
+	  // std::cout << "Vous avez appuyez sur GAUCHE {A} !" << std::endl;
 	  eventReceiver::lastPressedKey = eventReceiver::inputCase::KEY_KEY_A;
 	  return true;
 
 	case irr::KEY_KEY_D: // droite
 	  //input = 4;
-	  std::cout << "Vous avez appuyez sur DROITE {D} !" << std::endl;
+	  // std::cout << "Vous avez appuyez sur DROITE {D} !" << std::endl;
 	  eventReceiver::lastPressedKey = eventReceiver::inputCase::KEY_KEY_D;
 	  return true;
 	case irr::KEY_KEY_X: // pauser une bombe
-	  //input = 5;
-	  std::cout << "Vous avez poser une bombe {X} !" << std::endl;
+	  //std::cout << "Vous avez poser une bombe {X} !" << std::endl;
 	  eventReceiver::lastPressedKey = eventReceiver::inputCase::KEY_KEY_X;
 	  return true;
 	default:
-
-	  //input = 6;
 	  break;
 	}
     }
--- source/engine/world3d/world3d.hpp
+++ source/engine/world3d/world3d.hpp
@@ -5,7 +5,7 @@
 // Login   <sofiane@epitech.net>
 //
 // Started on  Thu Jun 15 16:01:22 2017 sofiane
-// Last update Sun Jun 18 17:13:12 2017 sofiane
+// Last update Sun Jun 18 18:43:08 2017 sofiane
 //
 
 #ifndef _WORLD3D_HPP_
@@ -20,22 +20,27 @@ class					World3d
   enum	Animation
     {
       STAND = irr::scene::EMAT_STAND,
-      RUN =  irr::scene::EMAT_STAND,
+      RUN =  irr::scene::EMAT_RUN,
     };
 public:
+  irr::scene::IAnimatedMeshSceneNode	*m_mesh;
+  irr::scene::ICameraSceneNode		*m_cam;
+
   World3d(irr::scene::IAnimatedMeshSceneNode *modele);
   ~World3d();
+
   // CAMERA
   irr::scene::ICameraSceneNode		*addCamera(Window *win, const int x, const int y, const int z);
-  // CREATE MESH
-  irr::scene::IAnimatedMeshSceneNode	*createMesh(Window *win, const irr::io::path &mesh, const irr::io::path &texture, const int x, const int y, const int z);
   // SET ANIMATION
   irr::scene::IAnimatedMeshSceneNode    *setAnimation(irr::scene::EMD2_ANIMATION_TYPE anim, irr::f32 speed, irr::scene::IAnimatedMeshSceneNode *node);
+    // CREATE MESH
+  irr::scene::IAnimatedMeshSceneNode	*createMesh(Window *win, const irr::io::path &mesh, const irr::io::path &texture, const int x, const int y, const int z);
+  //REMOVE MESH
   irr::scene::IAnimatedMeshSceneNode	*removeMesh(Window *win);
 
   irr::scene::ICameraSceneNode		*setCameraPosition(const int x, const int y, const int z);
-  irr::scene::IAnimatedMeshSceneNode	*m_mesh;
-  irr::scene::ICameraSceneNode		*m_cam;
+
+
 
 };
 
